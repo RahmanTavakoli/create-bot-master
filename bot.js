d1139a7d4c62bc63642f9a98afabc2d798d6d466
RahmN <rahmanxrf@gmail.com>
2024-06-27 00:29:59 +0330

    debug4

--- bot.js
+++ bot.js
@@ -1,17 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api');
-const { token } = require('./config');
 const { myApp } = require('./config');
+const { token } = require('./config');
+const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
-
-// مدیریت خطاهای عمومی
-bot.on('polling_error', (error) => {
-    console.error(`Polling error: ${error.code}`, error);
-});
-
-bot.on('webhook_error', (error) => {
-    console.error(`Webhook error: ${error.code}`, error);
-});
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
@@ -26,7 +17,7 @@ bot.onText(/\/start/, (msg) => {
                 [
                     {
                         text: "باز کردن برنامه",
-                        callback_data: "open_app"
+                        url: myApp // آدرس لینک مورد نظر خود را اینجا وارد کنید
                     }
                 ]
             ]
@@ -35,23 +26,9 @@ bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(chatId, welcomeMessage).then(() => {
         bot.sendMessage(chatId, personalizedMessage, options);
-    }).catch((error) => {
-        console.error(`Error sending message: ${error.message}`);
     });
 });
 
-bot.on('callback_query', (callbackQuery) => {
-    const msg = callbackQuery.message;
-    const chatId = msg.chat.id;
-
-    if (callbackQuery.data === 'open_app') {
-        bot.sendMessage(chatId, 'برای باز کردن برنامه، [اینجا کلیک کنید]()', { parse_mode: 'Markdown' }).catch((error) => {
-            console.error(`Error sending message: ${error.message}`);
-        });
-    }
-});
-
-// مدیریت پیام‌های دیگر و نمایش لینک
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
@@ -62,15 +39,13 @@ bot.on('message', (msg) => {
                     [
                         {
                             text: "Open ⬆️",
-                            url: "https://react-miniapp.onrender.com/"
+                            url: myApp // آدرس لینک مورد نظر خود را اینجا وارد کنید
                         }
                     ]
                 ]
             }
         };
 
-        bot.sendMessage(chatId, "برای باز کردن برنامه دکمه Open را لمس کنید:", options).catch((error) => {
-            console.error(`Error sending message: ${error.message}`);
-        });
+        bot.sendMessage(chatId, "برای باز کردن برنامه دکمه Open را لمس کنید:", options);
     }
 });
