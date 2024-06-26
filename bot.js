c44e04176afd5564ca195ab0747da622c2fc1540
RahmN <rahmanxrf@gmail.com>
2024-06-27 00:21:06 +0330

    debug3

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
+const TelegramBot = require('node-telegram-bot-api');
 const { token } = require('./config');
 const { myApp } = require('./config');
-const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -45,7 +45,7 @@ bot.on('callback_query', (callbackQuery) => {
     const chatId = msg.chat.id;
 
     if (callbackQuery.data === 'open_app') {
-        bot.sendMessage(chatId, 'برای باز کردن برنامه، [اینجا کلیک کنید](https://rezanuts.ir)', { parse_mode: 'Markdown' }).catch((error) => {
+        bot.sendMessage(chatId, 'برای باز کردن برنامه، [اینجا کلیک کنید]()', { parse_mode: 'Markdown' }).catch((error) => {
             console.error(`Error sending message: ${error.message}`);
         });
     }
@@ -62,7 +62,7 @@ bot.on('message', (msg) => {
                     [
                         {
                             text: "Open ⬆️",
-                            url: myApp
+                            url: "https://react-miniapp.onrender.com/"
                         }
                     ]
                 ]
